740573786f8313e69dceca9ef1ccfba140efcfee
jgriel <jgriel1@fandm.edu>
2024-04-11 17:22:45 -0400

    init

---
+++ stream.py
@@ -0,0 +1,9 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+if "__name__" == "__main__":
+    song = AudioSegment.from_mp3("data/XP.mp3")
+    print('playing sound using  pydub')
+    play(song)
+    
+
